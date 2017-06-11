b7a47c9ea4fa9e565f5b72f06a6d61d8d4836d5e
xiaozi0lei <xiaozi0lei@163.com>
2017-06-11 18:07:52 +0800

    update build.gradle

--- src/main/java/cn/yingnote/wanying/main/Main.kt
+++ src/main/java/cn/yingnote/wanying/main/Main.kt
@@ -4,12 +4,16 @@ import cn.yingnote.wanying.filter.configFilter
 import cn.yingnote.wanying.router.configRouter
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import org.slf4j.LoggerFactory
 
 /**
  * Created by guolei on 2017/5/25.
  * 主函数入口
  */
 fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger("main")
+
+    // 初始化服务器全局配置
     val vertx: Vertx = Vertx.vertx()
     val server = vertx.createHttpServer()
     val router = Router.router(vertx)
@@ -18,12 +22,15 @@ fun main(args: Array<String>) {
     // 配置路由
     configRouter(router)
 
+    // 请求 handler
     server.requestHandler({ router.accept(it) })
+
+    // 监听端口，校验启动成功还是失败，如失败，打印错误信息
     server.listen(8080, { response ->
         if (response.succeeded()) {
-            println("启动成功！")
+            logger.debug("启动成功！")
         } else {
-            println("启动失败！" + response.cause())
+            logger.debug("启动失败！" + response.cause())
         }
     })
 }
